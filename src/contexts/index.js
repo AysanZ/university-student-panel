2a186a0ea39884e35b085d8d61b2b3e3a7d2cdc0
Aysanz <aysanzeinalvand@yahoo.com>
2023-06-21 20:47:17 +0330

    Kanban

---
+++ src/contexts/index.js
@@ -0,0 +1,65 @@
+export const boardData = {
+  columns: [
+    {
+      id: 1,
+      title: "Backlog",
+      cards: [
+        {
+          id: 1,
+          title: "Database Setup",
+          description: "Firebase Integration"
+        },
+        {
+          id: 2,
+          title: "Data Flow",
+          description: "Setup Diagram with other developers"
+        },
+      ]
+    },
+    {
+      id: 2,
+      title: "TODO",
+      cards: [
+        {
+          id: 9,
+          title: "Data Table Page",
+          description: "Server side Pagination",
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: "Doing",
+      cards: [
+        {
+          id: 10,
+          title: "Full Calendar Extension",
+          description: "Make new events and store in global states"
+        },
+        {
+          id: 11,
+          title: "Custom Kanban Board",
+          description: "Setup react-kanban dep within Dashboard as seperate page"
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: "Completed",
+      cards: [
+        {
+          id: 12,
+          title: "Vite Server Setup",
+          description: "Configure required modules and starters"
+        },
+        {
+          id: 13,
+          title: "Modular structre",
+          description: "Write css in form of modules to reduce the naming conflicts"
+        }
+      ]
+    }
+  ]
+}
+
+
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,9 +1,137 @@
-import React from 'react'
+import Board, { moveCard, moveColumn, removeCard, addCard } from '@asseinfo/react-kanban'
+import "@asseinfo/react-kanban/dist/styles.css";
+import useBoard from '../contexts/Board';
+import "./styles/Board.css"
+import { RxCross2 } from 'react-icons/rx'
+import { IoMdAdd } from 'react-icons/io'
+import AddCardModal from "../components/AddCardModal/AddCardModal";
+import { useState } from 'react';
+import { Header } from '../components';
 
-function Kanban() {
-  return (
-    <div>K</div>
-  )
+
+const Kanban = () => {
+
+  const [modalOpened, setModalOpened] = useState(false)
+
+
+    const { board, setBoard } = useBoard()
+
+    const handleColumnMove = (_card, source, destination) => {
+        const updatedBoard = moveColumn(board, source, destination)
+        setBoard(updatedBoard)
+    }
+
+    const handleCardMove = (_card, source, destination) => {
+        const updatedBoard = moveCard(board, source, destination)
+        setBoard(updatedBoard)
+
+    }
+
+    const getColumn = (card) => {
+        const column = board.columns.filter((column) => column.cards.includes(card))
+        
+        return column[0]
+    }
+
+    const getGradient = (card) => {
+        const column = getColumn(card)
+        const title = column.title
+        if (title === "TODO") {
+            return {
+                background:
+                    "rgba(145, 133, 223, 0.7)",
+                    color: "white",
+            };
+        } else if (title === "Doing") {
+            return {
+                background:
+                    "rgba(35, 169, 242, 0.7)",
+                    color: "white",
+            };
+        } else if (title === "Completed") {
+            return {
+                background:
+                    "rgba(28, 209, 80, 0.7)",
+                    color: "white",
+            };
+        } else if (title === "Backlog") {
+            return {
+                background:
+                    "rgba(231, 65, 52, 0.7)",
+                    color: "white",
+            };
+        }
+    }
+
+
+    return (
+      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Kanban" />
+        <div className="board-container">
+            <Board
+                allowAddColumn
+                allowRenameColumn
+                allowRemoveCard
+                onCardDragEnd={handleCardMove}
+                onColumnDragEnd={handleColumnMove}
+                renderCard={(props) => (
+                    <div className='kanban-card' style={getGradient(props)}>
+                        <div>
+                            <span>
+                                {props.title}
+                            </span>
+                            <button className='remove-button' type='button'
+                                onClick={() => {
+                                    const updatedBoard = removeCard(board,
+                                        getColumn(props),
+                                        props
+                                    )
+                                    setBoard(updatedBoard)
+                                }}
+                            >
+                                <RxCross2 color="white" size={15} />
+                            </button>
+                        </div>
+                        <span>{props.description}</span>
+                    </div>
+                )}
+                renderColumnHeader={(props) => {
+
+
+                    const handleCardAdd = (title, detail)=> {
+                        const card = {
+                            id : new Date().getTime(),
+                            title, 
+                            description: detail
+                        };
+
+                        const updatedBoard = addCard(board, props, card)
+                        setBoard(updatedBoard)
+                        setModalOpened(false)
+
+                    }
+
+                    return (
+                        <div className='column-header'>
+                            <span>{props.title}</span>
+
+                            <IoMdAdd
+                                color="#9185DF"
+                                size={25} title="Add card"
+                                onClick={()=>setModalOpened(true)}
+                            />
+                            <AddCardModal visible={modalOpened} handleCardAdd={handleCardAdd}
+                                onClose={() => setModalOpened(false)} />
+                        </div>
+                    )
+                }}
+
+            >
+                {board}
+            </Board>
+        </div>
+        </div>
+    )
 }
 
 export default Kanban
