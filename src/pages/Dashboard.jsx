37d9e75f066dbf6a8b230c63999dc86b1712923b
Aysanz <aysanzeinalvand@yahoo.com>
2023-06-23 16:26:08 +0330

    Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,61 +1,69 @@
 import React from "react";
 import { useStateContext } from "../contexts/ContextProvider";
 import lessons from "../data/lessons.json";
-
+import { LineChart } from "../components";
 import css from "./styles/Dashboard.module.css";
 
-function Dashboard() {
+const Dashboard = () => {
   const { currentColor, currentMode } = useStateContext();
 
   return (
     <div className={css.wrapper}>
       <div className={css.statistics}>
-        <div className={`bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ${css.static}`}>
+        <div
+          className={`bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ${css.static}`}
+        >
           <div>
             <span>All Courses</span>
             <span style={{ color: currentColor }}>04/06</span>
           </div>
           <img src="./images/dash1.svg" alt="student" />
         </div>
-        <div className={`bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ${css.static}`}>
+        <div
+          className={`bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ${css.static}`}
+        >
           <div>
             <span>All Projects</span>
             <span style={{ color: currentColor }}>40/60</span>
           </div>
           <img src="./images/dash2.svg" alt="student" />
         </div>
-        <div className={`bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ${css.static}`}>
+        <div
+          className={`bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ${css.static}`}
+        >
           <div>
-            <span>Test Attended
-</span>
+            <span>Test Attended</span>
             <span style={{ color: currentColor }}>30/50</span>
           </div>
           <img src="./images/dash3.svg" alt="student" />
         </div>
-        <div className={`bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ${css.static}`}>
+        <div
+          className={`bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ${css.static}`}
+        >
           <div>
-            <span>Test Passed
-</span>
+            <span>Test Passed</span>
             <span style={{ color: currentColor }}>15/20</span>
           </div>
           <img src="./images/dash4.svg" alt="student" />
         </div>
       </div>
 
-      <div className={css.container}>
-        <div className={css.header}>Sudent Activity</div>
+
+      <div className={`bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ${css.container}`}>
+        <div className={css.header}>Today's Lessons</div>
         {lessons.map((item, i) => (
           <div className={css.lesson}>
-            <div>
-              <span>{item.course}</span>
+              <span style={{backgroundColor: currentColor}}>{item.course}</span>
               <span>
-                <span>{item.time}</span>
+                <span >{item.time}</span>
                 <span>{item.class} </span>
               </span>
-            </div>
           </div>
         ))}
       </div>
+
+      <LineChart/>
+
     </div>
   );
 }
